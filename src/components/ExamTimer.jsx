c58df1a6b5397d239171793d6f25aba94363adfa
ranz0586 <ryanbanlawe@gmail.com>
2026-06-15 01:53:06 +0800

    Updated timer

--- src/components/ExamTimer.jsx
+++ src/components/ExamTimer.jsx
@@ -1,44 +1,70 @@
-import { useEffect } from "react";
+import { useEffect }
+from "react";
 
 export default function ExamTimer({
   seconds,
   setSeconds
 }) {
+
   useEffect(() => {
+
     const timer =
       setInterval(() => {
+
         setSeconds(
-          (prev) => prev - 1
+          prev =>
+            prev > 0
+              ? prev - 1
+              : 0
         );
+
       }, 1000);
 
     return () =>
-      clearInterval(timer);
-  }, []);
+      clearInterval(
+        timer
+      );
+
+  }, [setSeconds]);
 
   const mins =
-    Math.floor(seconds / 60);
+    Math.floor(
+      seconds / 60
+    );
 
   const secs =
     seconds % 60;
 
   return (
+
     <div
       style={{
-        background: "#fee2e2",
+        background:
+          "#fee2e2",
+
         padding: 12,
+
         borderRadius: 8,
+
         marginBottom: 20
       }}
     >
+
       Time Remaining:
 
       {" "}
 
       {mins}:
+
       {secs
         .toString()
-        .padStart(2, "0")}
+        .padStart(
+          2,
+          "0"
+        )}
+
     </div>
+
   );
+
 }
